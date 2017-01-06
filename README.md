771819e36d8cfc1e098c4d3ca674d755d3ee132f
KaiL4eK <darklexi494@gmail.com>
2017-01-06 21:10:07 +0300

    Qt5 compilation fixed

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # arm-linux-hf_cross
 docker: https://hub.docker.com/r/kail4ek/arm-linux-hf_cross/
+
+To set correct sysroot of RPi usage described here: https://sysprogs.com/w/fixing-rpath-link-issues-with-cross-compilers/  
+
+In short:  
+	- Set -rpath-link  
+	- Set direct paths to $SYSROOT/etc/ld.so.conf
--- qt5_compile_arm/config-arm.sh
+++ qt5_compile_arm/config-arm.sh
@@ -8,9 +8,18 @@ unset AS
 unset LD
 unset CXXFLAGS
 unset CFLAGS
-unset LD_LIBRARY_PATH
 
-./configure -prefix ~/qt-arm -release -device linux-rasp-pi-g++ -make libs -device-option CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- \
-		-opensource -confirm-license -platform linux-g++ -xplatform linux-arm-gnueabihf-g++ -nomake examples -nomake tests -optimized-qmake \
-		-skip qtwebkit -prefix ~/qt-arm -sysroot $HOME/sysroot
+export SYSROOT=$HOME/sysroot
+
+./configure -prefix ~/qt-arm -release \
+		-device linux-rasp-pi-g++ -make libs -device-option CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- \
+		-opensource -confirm-license \
+		-platform linux-g++ -xplatform linux-arm-gnueabihf-g++ \
+		-nomake examples -nomake tests -optimized-qmake \
+		-skip qtwebkit \
+		-reduce-exports \
+		-opengl es2 \
+		-prefix ~/qt-arm \
+		-qt-xcb \
+		-sysroot $SYSROOT -v
 
