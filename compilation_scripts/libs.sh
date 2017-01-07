62a0d148960e877d0a41f50e023c57e567a07260
KaiL4eK <darklexi494@gmail.com>
2017-01-07 14:48:43 +0300

    TBB_LIB_DIR fixed

--- compilation_scripts/libs.sh
+++ compilation_scripts/libs.sh
@@ -9,7 +9,7 @@ EIGEN_INCLUDE_DIR=$EIGEN_INSTALL_DIR/include/eigen2
 TBB_ROOT=$libraries_directory/tbb-src
 TBB_INSTALL_DIR=$libraries_directory/tbb-arm
 TBB_INCLUDE_DIR=$TBB_INSTALL_DIR/include
-TBB_LIB_DIR=$TBB_INSTALL_DIR/lib/libtbb
+TBB_LIB_DIR=$TBB_INSTALL_DIR/lib
 TBB_STDDEF_PATH=$TBB_INSTALL_DIR/include/tbb/tbb_stddef.h
 
 OPENCV_INSTALL_DIR=$libraries_directory/opencv-arm
